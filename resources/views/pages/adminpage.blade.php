8ee5b9feb5a2f65f1532667bfa725968615a1443
W Feenstra <wypkefeenstra1992@live.nl>
2018-06-20 13:00:39 +0200

    admin pagina 0.1
    action added to add interests and specialities

--- resources/views/pages/adminpage.blade.php
+++ resources/views/pages/adminpage.blade.php
@@ -10,11 +10,19 @@
         <div class="col-md-8">
             <div class="card">
                 <h5>interesses toevoegen</h5>
-                    {{Form::text('addinterest', '', ['class' => 'form-control', 'placeholder' => 'left'])}}
+                {!! Form::open(['action' => 'PagesController@addInterest' , 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                    <div class="form-group">
+                    {{Form::text('addInterest', '', ['class' => 'form-control', 'placeholder' => ''])}}
                     {{Form::submit('Interesse toevoegen', ['class'=>'btn btn-primary float right'])}}
+                    </div>
+                {!! Form::close() !!}
                 <h5>specialiteiten toevoegen</h5>
-                    {{Form::text('addspeciality', '', ['class' => 'form-control', 'placeholder' => 'left'])}}
+                {!! Form::open(['action' => 'PagesController@update', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                    <div class="form-group">
+                    {{Form::text('addSpeciality', '', ['class' => 'form-control', 'placeholder' => ''])}}
                     {{Form::submit('Specialiteiten toevoegen', ['class'=>'btn btn-primary float right'])}}
+                    </div>
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
